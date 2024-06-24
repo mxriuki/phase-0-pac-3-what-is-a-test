f10d038c6c8a69133754e87ab6f661cff0d468ef
mxriuki <mwendav37@gmail.com>
2024-06-24 22:15:42 +0300

    completeAssignment

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+
 const name = "Joe";
 const height = 74;
 const message = `${name} is ${height} inches tall`;
+const name = 'Susan'
+const height = 20
+const message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message};
+module.exports = { name, height, message }
+
+console.log(message)
